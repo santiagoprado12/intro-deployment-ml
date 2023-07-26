2cc45064407e0f215c232eb5f8b38f93294c08a4
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-07-26 09:49:31 -0500

    remove dot env

--- src/prepare.py
+++ src/prepare.py
@@ -3,9 +3,7 @@ import pandas as pd
 from io import StringIO
 import sys
 import logging
-from dotenv import load_dotenv
 
-load_dotenv()
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s:%(name)s: %(message)s',
